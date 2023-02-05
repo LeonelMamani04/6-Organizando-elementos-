d1a680e193e1f0221ecf2722d2801c36c995f9c8
LeonelMamani04 <leo6633nelmail.com>
2023-02-04 22:14:41 -0300

    6-Organizando-elementos

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3004;
+
+app.listen(PORT, () => 
+console.log(`Server lister in port ${PORT} 
+http://localhost:${PORT}`));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+});
